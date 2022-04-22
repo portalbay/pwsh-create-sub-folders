28288ee69b54fb1e00b7076ba565717fabf1b397
scv_ready <74455344+portalbay@users.noreply.github.com>
2022-04-22 03:37:18 -0400

    Update updatePersonnelStruct.ps1

--- updatePersonnelStruct.ps1
+++ updatePersonnelStruct.ps1
@@ -1,17 +1,18 @@
-$hr_path = "C:/project_path/code/Personnel File"
+$root_path = "C:/project_path/code/Personnel File"
+$new_folder_name="Example"
 
  
 
-$employee_dirs = Get-ChildItem  -Directory -Path $hr_path| Select-Object Name
+$employee_dirs = Get-ChildItem  -Directory -Path $root_path| Select-Object Name
 
 foreach ($e in $employee_dirs){
     $o_path = $e.Name
     $o_result = "" 
-    $new_directory = $hr_path + "\" + $e.Name + "\Discipline"
+    $new_directory = $root_path + "\" + $e.Name + "\" + $new_folder_name
     #Write-Host($e.Name + " -- " + $new_directory)
 
     If (!(test-path $new_directory)){
-        $o_result = "New: \Discipline"
+        $o_result = "New: \" + $new_folder_name
         New-Item -ItemType Directory -Path $new_directory | Out-Null
     }
     else {
